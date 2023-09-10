776b34669e9d2ba6189d7a14ce87c99fe332c8db
Addel <sigmamediacode@gmail.com>
2023-09-10 12:10:33 -0400

    cambiando el link de los fetch

--- src/servicios/productos-servicios.js
+++ src/servicios/productos-servicios.js
@@ -28,13 +28,13 @@
 
 // Fetch API 
 const listaProductos = () => {
-  return fetch('http://localhost:3000/producto').then( respuesta => {
+  return fetch('https://alurageek-moil.onrender.com').then( respuesta => {
     return respuesta.json()  
   });
 };
 
 const crearProducto = (imgUrl, recomendado, nombre, precio) => {
-  return fetch('http://localhost:3000/producto', {
+  return fetch('https://alurageek-moil.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ const crearProducto = (imgUrl, recomendado, nombre, precio) => {
 
 const eliminarProducto = (id) => {
  
-  return fetch(`http://localhost:3000/producto/${id}`, {
+  return fetch(`https://alurageek-moil.onrender.com/${id}`, {
     method: 'DELETE',
   })
     .then((response) => {
@@ -68,18 +68,18 @@ function refrescarPagina() {
 
 
 const detalleProducto = (id) => {
-  return fetch(`http://localhost:3000/producto/${id}`).then((respuesta) =>
+  return fetch(`https://alurageek-moil.onrender.com/${id}`).then((respuesta) =>
     respuesta.json()
   );
 };
 
 const editarProducto = (id) => {
-  return fetch(`http://localhost:3000/producto/${id}`).then((respuesta) => respuesta.json()
+  return fetch(`https://alurageek-moil.onrender.com/${id}`).then((respuesta) => respuesta.json()
   );
 };
 
 const actualizarProducto = (imgUrl, recomendado, nombre, precio, id) => {
-  return fetch(`http://localhost:3000/producto/${id}`, {
+  return fetch(`https://alurageek-moil.onrender.com/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
